33269ca1b7b915645cd069002e849d507a97ca20
iman.112 <91781722+iman-112@users.noreply.github.com>
2022-12-09 18:59:12 +0400

    v1.4.9

--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -1,12 +1,24 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { GiNetworkBars } from 'react-icons/gi'
+
 
 const Logo = () => {
   return (
     < >
    
-    <Link href="/" className='text-[2rem] font-caveot font-bold flex items-center'> CansWeb   <span className="text-[11px] text-slate-400 pointer-events-none ml-1 items-end mt-3">v1.4.8</span>  </Link> 
+
+
+   <Link href="/" >
+    <div   className='relative'> 
+      
+    <Image src="/images/cans-logo.png"  alt="" height={100} width={130} className="pointer-events-none"  />  
+ 
+      </div>  
+   
+      </Link>
+   
+
     
     </>
   )
