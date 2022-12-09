deb00e6d212a89c249d10082ebc438ec5d255ebe
iman.112 <91781722+iman-112@users.noreply.github.com>
2022-12-09 17:39:24 +0400

    v1.4.8

--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     < >
    
-    <Link href="/" className='text-[2rem] font-caveot font-bold flex items-center'> CansWeb   <span className="text-[11px] text-slate-400 pointer-events-none ml-1 items-end mt-3">v1.4.6</span>  </Link> 
+    <Link href="/" className='text-[2rem] font-caveot font-bold flex items-center'> CansWeb   <span className="text-[11px] text-slate-400 pointer-events-none ml-1 items-end mt-3">v1.4.8</span>  </Link> 
     
     </>
   )
